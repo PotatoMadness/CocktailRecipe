91568a031d4cfce4b34e7db40e017b8de84ed561
suhyun choi <shchoi@soft-berry.com>
2024-05-24 17:24:26 +0900

    feat : build-logic 추가

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,15 @@
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    `kotlin-dsl`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -11,4 +11,5 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.jetbrainsKotlinJvm) apply false
 }
