6ab04b479d1f3bb2ffd6359270324132af146465
suhyun choi <shchoi@soft-berry.com>
2024-06-03 18:02:17 +0900

    fix : custom recipe load error

--- core/data/src/main/java/com/potatomadness/data/dao/CocktailDao.kt
+++ core/data/src/main/java/com/potatomadness/data/dao/CocktailDao.kt
@@ -7,21 +7,22 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
 import androidx.room.Update
+import com.potatomadness.data.model.Cocktail
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CocktailDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(cocktail: com.potatomadness.data.model.Cocktail): Long
+    suspend fun insert(cocktail: Cocktail): Long
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertFullInfo(cocktail: com.potatomadness.data.model.Cocktail)
+    suspend fun insertFullInfo(cocktail: Cocktail)
 
     @Update
-    suspend fun update(cocktail: com.potatomadness.data.model.Cocktail)
+    suspend fun update(cocktail: Cocktail)
 
     @Transaction
-    suspend fun upsert(cocktail: com.potatomadness.data.model.Cocktail) {
+    suspend fun upsert(cocktail: Cocktail) {
         val id = insert(cocktail = cocktail)
         if(id == -1L && cocktail.isSimplySaved()) {
             update(cocktail)
@@ -29,20 +30,26 @@ interface CocktailDao {
     }
 
     @Delete
-    suspend fun delete(cocktail: com.potatomadness.data.model.Cocktail)
+    suspend fun delete(cocktail: Cocktail)
 
     @Query("SELECT EXISTS(SELECT 1 FROM Cocktail WHERE id = :cocktailId and isFavorite != 0 LIMIT 1)")
     fun isFavorite(cocktailId: Int): Flow<Boolean>
 
     @Query("SELECT * FROM Cocktail")
-    fun getAll(): Flow<List<com.potatomadness.data.model.Cocktail>>
+    fun getAll(): Flow<List<Cocktail>>
 
     @Query("SELECT * FROM Cocktail WHERE isCustom != 0")
-    fun getMyRecipes(): Flow<List<com.potatomadness.data.model.Cocktail>>
+    fun getMyRecipes(): Flow<List<Cocktail>>
 
     @Query("SELECT * FROM Cocktail WHERE isFavorite != 0")
-    fun getFavoriteRecipes(): Flow<List<com.potatomadness.data.model.Cocktail>>
+    fun getFavoriteRecipes(): Flow<List<Cocktail>>
 
     @Query("SELECT EXISTS(SELECT 1 FROM Cocktail WHERE name = :name)")
     suspend fun isExist(name: String): Boolean
+
+    @Query("SELECT EXISTS(SELECT 1 FROM Cocktail WHERE id = :id)")
+    suspend fun isExist(id: Int): Boolean
+
+    @Query("SELECT * FROM Cocktail WHERE id = :id")
+    suspend fun getRecipeById(id: Int): Cocktail
 }
